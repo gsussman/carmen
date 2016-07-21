20760337a8511c757bf7b8343fd6d12764b34ccb
GeneAMS <gene.sussman@gmail.com>
2016-07-21 03:23:10 -0400

    import taken out

--- travelapp/settings.py
+++ travelapp/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,10 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'signup',
     'frontpage',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
 ]
 
 MIDDLEWARE_CLASSES = [
